123cfa3db676f297963812cc8306ca6fb71b68a0
HamadAljneibi <147314961+HamadAljneibi@users.noreply.github.com>
2023-10-22 15:03:46 +0400

    Update ap2.java

--- ap2.java
+++ ap2.java
@@ -1,1 +1,16 @@
 // hi
+
+// write aprogramme that the sentation of the number( 1,2,3,) in word
+
+pubic class main
+  {
+    int x=1;
+
+    if (x==1)
+        {
+
+      
+switch(x)
+  {
+    case 1:
+      system.out
